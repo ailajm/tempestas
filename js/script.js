7f91cf1119e000b24eea416c2cf730d4e8d8bea3
cotymckinney <saucermenwebdesign@gmail.com>
2021-02-28 00:54:30 -0500

    functional with placeholder data

--- js/script.js
+++ js/script.js
@@ -7,46 +7,41 @@ let weatherData, userInput;
 // Cached element references - selected DOM elements
 const $location = $('#returnLocale');
 const $temperature = $('#temperature');
-const $windChill = $('#windChill');
+const $feelsLike = $('#feelsLike');
 const $weather = $('#weather');
 const appID = 'db8cd329e3c02194b1ef2f746711a5cd';
-const $input = $('input[type="text"]');
+let $input = $('input[type="text"]');
 
 // Event Listeners
 $('form').on('submit', handleGetData);
 
-    // Sets DOM elements with data fetched by AJAX request
 $.ajax({
-    url:'https://api.openweathermap.org/data/2.5/weather?q=louisville&appid=' + appID
+    url:`https://api.openweathermap.org/data/2.5/weather?q=louisville&units=imperial&appid=${appID}`
   })
   .then(
     (data) => {
       $location.text(data.name);
       $temperature.text(data.main.temp);
-      $windChill.text(data.main.feels_like);
-      $weather.text(data.weather.id);
+      $feelsLike.text(data.main.feels_like);
+      $weather.text(data.weather.description);
     },
     (error) => {
       console.log('bad request: ', error);
     });
 
-// Functions
-    // Assigns data from AJAX request to 
 function render() {
         $location.text(weatherData.name);
         $temperature.text(weatherData.main.temp);
-        $windChill.text(weatherData.main.feels_like);   
+        $feelsLike.text(weatherData.main.feels_like);   
         $weather.text(weatherData.weather.description);   
     };  
 
- function handleGetData(event) {
-    event.preventDefault();
-        // calling preventDefault() on a 'submit' event will prevent a page refresh  
+ function handleGetData(e) {
+    e.preventDefault();
     userInput = $input.val();
-        // getting the user input
     $.ajax({
-        url:'https://api.openweathermap.org/data/2.5/weather?q=louisville&appid=' + appID
-        }).then(
+        url:`https://api.openweathermap.org/data/2.5/weather?q=${$input}&units=imperial&appid=${appID}`
+    }).then(
         (data) => {
             weatherData = data;
             render();
@@ -55,7 +50,7 @@ function render() {
             console.log('bad request', error);
         }
     )   
-}
+};
 
 
     
